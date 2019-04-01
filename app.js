7dfc46fd12ce0235c3b89accb08ebe562a36ac5a
GoldenFlash <892458829@qq.com>
2019-04-01 23:02:11 +0800

    .

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
 var superagent = require('superagent');
 var cheerio = require('cheerio');
 var fs = require('fs');
- 
-var url = 'https://coding.imooc.com/class/326.html?mc_marking=c5a3de541d57c8b0f8adb42c41479892&mc_channel=syb11'
+var path = require("path")
+var url = 'http://m.iqiyi.com/v_19rsiezzrg.html'
 var savePath = '保存文件路径'
  
 superagent
     .get(url)
     .end(function(err, res) {
- 
+        // var path = path.join(__dirname,)
+        console.log("res",res.data)
+        var reg = /(\n|\r)/g
+        var data = res.text.replace(reg, "").replace(/\\/g,"")
+        // const $ = cheerio.load('<h1 class="aaa"><p>123</p><h1>')
+        const $ = cheerio.load(data)
+        var src = $('.c-openVip').text()
+        console.log("src",src)
+        fs.writeFile('html.txt', JSON.stringify(data), (err) => {
+            if (err) throw err;
+            console.log('文件已被保存');
+        });
     })
  
 // 获取视频 url
